1db72b18d33ad568fa0638273bd7e7b8867006d4
Gernot Feichter <gernot.feichter@bearingpoint.com>
2021-12-20 22:44:52 +0100

    feat: db initialisation

--- lib/db/init.dart
+++ lib/db/init.dart
@@ -1,1 +1,106 @@
-/**/
+import 'dart:developer';
+import 'dart:io';
+
+import 'package:path_provider/path_provider.dart';
+import 'package:json_schema2/json_schema2.dart';
+
+// normally I would load this from resource folder, but dart seems to not have
+// that feature anymore since its switch aot compilation
+// TODO: there could be a better way
+const seed = """
+{
+  "pistes": [
+    {
+      "name": "Zugang Skiroute Wilde Abfahrt Edelgriess (Rosmariestollen)",
+      "state": "closed",
+      "notification": true
+    },
+    {
+      "name": "Skitour Route Obertraun",
+      "state": "closed",
+      "notification": true
+    }
+  ]
+}
+""";
+
+const schemaString = """
+{
+  "\$schema": "http://json-schema.org/draft-06/schema#",
+  "\$ref": "#/definitions/Welcome",
+  "definitions": {
+    "Welcome": {
+      "type": "object",
+      "additionalProperties": false,
+      "properties": {
+        "pistes": {
+          "type": "array",
+          "items": {
+            "\$ref": "#/definitions/Piste"
+          }
+        }
+      },
+      "required": [
+        "pistes"
+      ],
+      "title": "Welcome"
+    },
+    "Piste": {
+      "type": "object",
+      "additionalProperties": false,
+      "properties": {
+        "name": {
+          "type": "string"
+        },
+        "state": {
+          "type": "string",
+          "enum": ["open", "closed", "warning", "unknown"]
+        },
+        "notification": {
+          "type": "boolean"
+        }
+      },
+      "required": [
+        "name",
+        "notification",
+        "state"
+      ],
+      "title": "Piste"
+    }
+  }
+}
+""";
+
+void init() async{
+  log("init db started");
+
+  final documentsDirectory = await getApplicationDocumentsDirectory();
+  final persistenceDirectory = "${documentsDirectory.path}/.dachstein_pistes";
+  log("persistenceDirectory = $persistenceDirectory");
+
+  final directory = Directory(persistenceDirectory);
+  if (directory.existsSync()) {
+    log("persistenceDirectory existed already");
+  } else {
+    log("creating persistenceDirectory");
+    directory.createSync(recursive: true);
+  }
+  final file = File(
+      "$persistenceDirectory/"
+      "persistence.json");
+  if (file.existsSync()) {
+    log("db already initialized");
+  } else {
+    log("initializing db");
+    final jsonSchema = JsonSchema.createSchema(schemaString);
+    if (jsonSchema.validate(seed, parseJson: true)) {
+      file.writeAsStringSync(seed);
+    } else {
+      log("schema validation error!", level: 3);
+      throw Exception("Dachstein Pistes could not start due to schema "
+          "validation error");
+    }
+  }
+
+  log("init db finished");
+}
--- lib/init/init.dart
+++ lib/init/init.dart
@@ -1,7 +1,11 @@
 import 'dart:developer';
+import 'package:dachstein_pistes/db/init.dart' as db;
 
 void init() {
   log("init started");
+
+  //
+  db.init();
 
   // setup alarm manager (androids way of cronjob)
 
