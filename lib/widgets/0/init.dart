c37e61c60103ad4a50c6371763d28ebe391de0a8
Gernot Feichter <gernot.feichter@bearingpoint.com>
2021-12-28 22:43:55 +0100

    feat: start GridView.count

--- lib/widgets/0/init.dart
+++ lib/widgets/0/init.dart
@@ -47,8 +47,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   late Future<AppSettings> appSettings;
+  List<int> myList = [1,2,3,4]; // TODO Gernot
 
   void _refreshState() {
     setState(() {
@@ -61,12 +61,11 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-
   @override
   void initState() {
     super.initState();
     appSettings = get();
-    setState(() {});
+    setState(() {}); // TODO Gernot
   }
 
   @override
@@ -78,49 +77,37 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            FutureBuilder<AppSettings>(
-              future: appSettings,
-              builder: (BuildContext context, AsyncSnapshot<AppSettings> snapshot) {
-                if (snapshot.hasData) {
-                  return Text(snapshot.data!.pistes.first.name);
-                } else {
-                  return const Text('Error fetching data from db');
-                }
-              }
-            )
-          ],
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _refreshState,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: GridView.count(
+          primary: false,
+          padding: const EdgeInsets.all(20),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          crossAxisCount: 1,
+          childAspectRatio: 15,
+          children: <Widget>[
+            for (var i in myList) Text(i.toString())
+          ],
+        )
+        // FutureBuilder<AppSettings>(
+        //   future: appSettings,
+        //   builder: (BuildContext context, AsyncSnapshot<AppSettings> snapshot) {
+        //     if (snapshot.hasData) {
+        //       return Text(snapshot.data!.pistes.first.name);
+        //     } else {
+        //       return const Text('Error fetching data from db');
+        //     }
+        //   }
+        // )
+        );
+    // floatingActionButton: FloatingActionButton(
+    //   onPressed: _refreshState,
+    //   tooltip: 'Increment',
+    //   child: const Icon(Icons.add),
+    // ), // This trailing comma makes auto-formatting nicer for build methods.
   }
 }
