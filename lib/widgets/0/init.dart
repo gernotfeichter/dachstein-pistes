fe9212cf24b1364c06b8343e7d418b4cbb689fa7
Gernot Feichter <gernot.feichter@bearingpoint.com>
2021-12-30 15:27:11 +0100

    feat: finish GridView.count

--- lib/widgets/0/init.dart
+++ lib/widgets/0/init.dart
@@ -1,6 +1,7 @@
 import 'package:dachstein_pistes/db/init.dart';
 import 'package:dachstein_pistes/db/model.dart';
 import 'package:dachstein_pistes/globals/init.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
@@ -13,15 +14,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: title,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: title),
@@ -31,16 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -70,35 +52,72 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text(widget.title),
         ),
         body: FutureBuilder<AppSettings>(
-              future: appSettings,
-              builder: (BuildContext context, AsyncSnapshot<AppSettings> snapshot) {
-                if (snapshot.hasData) {
-                  List<Widget> widgetList = [];
-                  widgetList.add(const Text("Piste", textAlign: TextAlign.center,));
-                  widgetList.add(const Text("State", textAlign: TextAlign.center,));
-                  widgetList.add(const Text("Notification", textAlign: TextAlign.center,));
-                  for (var piste in snapshot.data!.pistes) {
-                    widgetList.add(Text(piste.name, textAlign: TextAlign.center,));
-                    widgetList.add(Text(piste.state, textAlign: TextAlign.center,));
-                    widgetList.add(
-                        Checkbox(value: piste.notification,
-                          onChanged: (value) => _togglePisteNotification(snapshot.data, piste),
-                        ));
-                  }
-                  return GridView.count(
-                    primary: false,
-                    padding: const EdgeInsets.all(20),
-                    crossAxisSpacing: 10,
-                    mainAxisSpacing: 10,
-                    crossAxisCount: 3,
-                    childAspectRatio: 10,
-                    children: widgetList
+            future: appSettings,
+            builder:
+                (BuildContext context, AsyncSnapshot<AppSettings> snapshot) {
+              if (snapshot.hasData) {
+                List<Widget> widgetListHeader = [];
+                List<Widget> widgetListContent = [];
+                widgetListHeader.add(const Text(
+                  "Piste",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ));
+                widgetListHeader.add(const Text(
+                  "State",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ));
+                widgetListHeader.add(const Text(
+                  "Notification",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ));
+                for (var piste in snapshot.data!.pistes) {
+                  widgetListContent.add(Center(
+                      child: Text(
+                    piste.name,
+                    textAlign: TextAlign.start,
+                  )));
+                  widgetListContent.add(Center(
+                      child: Text(
+                    piste.state,
+                    textAlign: TextAlign.center,
+                  )));
+                  widgetListContent.add(
+                    Center(
+                        child: Checkbox(
+                      value: piste.notification,
+                      onChanged: (value) =>
+                          _togglePisteNotification(snapshot.data, piste),
+                    )),
                   );
-                } else {
-                  return const Text('Could not fetch pistes yet');
                 }
+                return Column(children: [
+                  Flexible(
+                      flex: 1,
+                      child: GridView.count(
+                          primary: false,
+                          padding: const EdgeInsets.all(20),
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 30,
+                          crossAxisCount: 3,
+                          childAspectRatio: 1.3,
+                          children: widgetListHeader)),
+                  Flexible(
+                      flex: 15,
+                      child: GridView.count(
+                          primary: false,
+                          padding: const EdgeInsets.all(20),
+                          crossAxisSpacing: 10,
+                          mainAxisSpacing: 30,
+                          crossAxisCount: 3,
+                          childAspectRatio: 1.3,
+                          children: widgetListContent))
+                ]);
+              } else {
+                return const Text('Could not fetch pistes yet');
               }
-            )
-        );
+            }));
   }
 }
