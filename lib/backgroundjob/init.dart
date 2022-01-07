1e0d63fa5cf8e387e2d6fc797cab52075b94f58d
Gernot Feichter <gernot.feichter@bearingpoint.com>
2022-01-07 23:39:24 +0100

    debug: log job to logcat

--- lib/backgroundjob/init.dart
+++ lib/backgroundjob/init.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 import 'dart:developer';
+import 'dart:io';
 import 'dart:isolate';
-
+import 'package:logging/logging.dart';
+import 'package:logging_to_logcat/logging_to_logcat.dart';
 import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:dachstein_pistes/db/init.dart';
 import 'package:dachstein_pistes/db/model.dart';
@@ -11,74 +13,95 @@ import 'package:html/parser.dart';
 import 'package:http/http.dart' as http;
 
 Future<void> job() async {
-  // fetch db state
-  AppSettings appSettings = await get();
+  Logger.root.activateLogcat();
+  final Logger log = Logger("DachsteinPistesJobLogger");
 
-  // fetch web state
-  const url =
-      'https://www.derdachstein.at/de/dachstein-aktuell/gletscherbericht';
-  http.Response httpResponse = /*responseInput ??*/ await http.get(Uri.parse(url));
-  log("response was ${httpResponse.body.substring(0,100)} (rest truncated)");
-  if (httpResponse.statusCode >= 200 && httpResponse.statusCode < 300) {
-    var document = parse(httpResponse.body);
-    var table = document
-        .getElementById('accordion-pisten-dachstein')!
-        .querySelector('table');
-    var tableData = table!.getElementsByTagName('td');
-    var counter = 0;
-    String currentPisteState = "unknown";
-    for (var element in tableData) {
-      if (counter % 4 == 0) {
-        if (element.innerHtml.contains("status bg-danger")) {
-          currentPisteState = "closed";
-        }
-        if (element.innerHtml.contains("status bg-success")) {
-          currentPisteState = "open";
-        }
-        if (element.innerHtml.contains("status bg-warning")) {
-          currentPisteState = "warning";
-        }
-      } else if (counter % 4 == 3) {
-        var currentPisteName = element.text.trim();
-        bool currentPisteNotification =
-            getPisteNotification(appSettings, currentPisteName);
-        var oldPiste = appSettings.pistes.firstWhere(
-          (element) => element.name == currentPisteName,
-          orElse: () {
-            return Piste(
-                name: currentPisteName, state: "unknown", notification: false);
-          },
-        );
-        var currentPiste = Piste(
-            name: currentPisteName,
-            state: currentPisteState,
-            notification: currentPisteNotification);
+  try {
+    log.info(
+        "job started: MainPageState.instanceSet=${MainPageState.instanceSet} "
+            "pid=$pid");
 
-        // and detect diff "web state" to "fetch db state"
-        if (oldPiste.state != currentPiste.state && currentPiste.notification) {
-          log("notification: Piste $currentPisteName changed from state "
-              "${oldPiste.state} to ${currentPiste.state}!");
-          notification.displayNotification(
-              title: "Dachstein Piste State Changed",
-              body: "${currentPiste.name} changed to ${currentPiste.state}");
-        }
+    // fetch db state
+    AppSettings appSettings = await get();
 
-        // update db
-        appSettings.pistes
-            .removeWhere((element) => element.name == currentPisteName);
-        appSettings.pistes.insert(0, currentPiste);
-        appSettings.refreshSettings.last = DateTime.now().toString();
+    // fetch web state
+    const url =
+        'https://www.derdachstein.at/de/dachstein-aktuell/gletscherbericht';
+    http.Response httpResponse = /*responseInput ??*/ await http.get(
+        Uri.parse(url));
+    log.info(
+        "response was ${httpResponse.body.substring(0, 100)} (rest truncated)");
+    if (httpResponse.statusCode >= 200 && httpResponse.statusCode < 300) {
+      var document = parse(httpResponse.body);
+      var table = document
+          .getElementById('accordion-pisten-dachstein')!
+          .querySelector('table');
+      var tableData = table!.getElementsByTagName('td');
+      var counter = 0;
+      String currentPisteState = "unknown";
+      for (var element in tableData) {
+        if (counter % 4 == 0) {
+          if (element.innerHtml.contains("status bg-danger")) {
+            currentPisteState = "closed";
+          }
+          if (element.innerHtml.contains("status bg-success")) {
+            currentPisteState = "open";
+          }
+          if (element.innerHtml.contains("status bg-warning")) {
+            currentPisteState = "warning";
+          }
+        } else if (counter % 4 == 3) {
+          var currentPisteName = element.text.trim();
+          bool currentPisteNotification =
+          getPisteNotification(appSettings, currentPisteName);
+          var oldPiste = appSettings.pistes.firstWhere(
+                (element) => element.name == currentPisteName,
+            orElse: () {
+              return Piste(
+                  name: currentPisteName,
+                  state: "unknown",
+                  notification: false);
+            },
+          );
+          var currentPiste = Piste(
+              name: currentPisteName,
+              state: currentPisteState,
+              notification: currentPisteNotification);
+
+          // and detect diff "web state" to "fetch db state"
+          log.info("detect diff");
+          if (oldPiste.state != currentPiste.state &&
+              currentPiste.notification) {
+            log.info("notification: Piste $currentPisteName changed from state "
+                "${oldPiste.state} to ${currentPiste.state}!");
+            notification.displayNotification(
+                title: "Dachstein Piste State Changed",
+                body: "${currentPiste.name} changed to ${currentPiste.state}");
+          }
+
+          // update db
+          log.info("prepare update db");
+          appSettings.pistes
+              .removeWhere((element) => element.name == currentPisteName);
+          appSettings.pistes.insert(0, currentPiste);
+          appSettings.refreshSettings.last = DateTime.now().toString();
+        }
+        counter++;
       }
-      counter++;
-    }
-    await set(appSettings);
+      log.info("update db");
+      await set(appSettings);
 
-    // notify main thread (ui)
-    notifyMainThread();
-  } else {
-    throw Exception("http get for url $url returned status "
-        "${httpResponse.statusCode} and "
-        "content ${httpResponse.body}");
+      // notify main thread (ui)
+      log.info("notifyMainThread");
+      notifyMainThread();
+      log.info("job finished successfully");
+    } else {
+      throw Exception("http get for url $url returned status "
+          "${httpResponse.statusCode} and "
+          "content ${httpResponse.body}");
+    }
+  } catch (e) {
+    log.warning("job failed with error ${e.toString()}");
   }
 }
 
